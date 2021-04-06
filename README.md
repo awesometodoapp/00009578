45341b1c4d347fad3f686564766059dd93176ebb
00009578 <Abdurakhmonov.Jakhongir@mail.ru>
2021-04-06 15:36:58 +0500

    For GiHub - Source Code Deployment

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# 00009578
- 
+# Awesome To Do App.
+
+### Description:
+This web applicatoin allows users to create, read, edit and delete tasks as to do list items. In order to create a task, just write inside the input and your task will be added once you click the 'Add' button. In case, you have written wrongly, you can edit any task text by clicking onto the specific task. All added tasks will be available to read in the home page. Moreover, 'Remove' button deletes unwanted task items while you can select tasks as done by clicking 'Complete' button. By using 'Complete All' button you can select all tasks as completed. Furthermore, you can review your finished tasks in the 'Completed Tasks' page. Additionally, any task that is added or completed will be assigned a current date and time respectively. Finally, this minimalistcally designed awesome to do app. is fully responsive repectively.
+
+### How to run the application locally:
+1.Download or clone the source code of the application:
+```bash
+git clone https://github.com/awesometodoapp/00009578.git
+```
+2.Install dependencies:
+```cmd
+npm install --save
+```
+### Run the application:
+```cmd
+node app.js
+```
+### Dependencies:
+- express
+- pug
+- body-parser
+- moment
+
+#### Source Code Link:
+https://github.com/awesometodoapp/00009578.git
---
+++ app.js
@@ -0,0 +1,89 @@
+const express = require('express')
+const path = require('path')
+const bodyParser = require('body-parser')
+const moment = require('moment')
+const app = express()
+
+app.set('view engine', 'pug')
+
+app.use( express.static( path.join(__dirname,'public')) )
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+let todoTasks = []
+let tasks = []
+let counter = 0
+
+app.get('/', (req,res) => {
+  tasks = []
+  todoTasks.forEach(function(item){
+    if (item.done === false) {
+      tasks.push(item)
+    }
+  })
+  res.render('./tasks', { tasks })
+})
+
+app.get('/completed', (req,res) => {
+  res.render('./completed', { todoTasks })
+})
+
+app.post('/tasks', (req,res) => {
+  let todo = {id: counter++, name: req.body.task, done: false, dateCreated: 'Created at ' + moment().format('MMMM Do YYYY, h:mm:ss a')}
+  todoTasks.push(todo)
+  res.redirect('/')
+})
+
+app.delete('/task/:id', (req,res) => {
+  todoTasks.forEach(function(item, index) {
+  	if (item.id === parseInt(req.params.id)) {
+  		todoTasks.splice(index, 1)
+  		return
+  	}
+  })
+  res.render('./tasks', { tasks })
+})
+
+app.put('/completed/:id', (req, res) => {
+  todoTasks.forEach(function(item, index) {
+  	if (item.id === parseInt(req.params.id)) {
+  		item.done = true
+      item.dateCompleted = 'Completed at ' + moment().format('MMMM Do YYYY, h:mm:ss a')
+  		return
+  	}
+  })
+  res.render('./tasks', { tasks })
+})
+
+app.put('/done-all', (req, res) => {
+  todoTasks.forEach(item => {
+    item.done = true
+    item.dateCompleted = 'Completed at ' + moment().format('MMMM Do YYYY, h:mm:ss a')
+  })
+  res.render('./tasks', { tasks })
+})
+
+
+app.put('/edited/:id/:taskname', (req, res) => {
+  let id = req.params.id
+  let taskname = req.params.taskname
+  todoTasks.forEach(function(item) {
+    if (item.id === parseInt(id)) {
+      item.name = taskname
+      return
+    }
+  })
+  res.render('./tasks', { tasks })
+})
+
+
+app.listen(3000, err => {
+	if(err) throw err
+
+	console.log('App is running on port 3000...')
+})
+
